946d34db186f8a544d2312addc3517085f60d79d
devdazzlee <bhaia9036@gmail.com>
2026-06-12 22:02:53 +0500

    Update

--- config/config.ts
+++ config/config.ts
@@ -1,4 +1,4 @@
-export const API_BASE_URL = "http://localhost:9000"
-// export const API_BASE_URL = "https://manpasand-pos-beta.vercel.app"
+// export const API_BASE_URL = "http://localhost:9000"
+export const API_BASE_URL = "https://yam-n7-dashbaord.vercel.app"
 export const API_VERSION = "/api/v1";
 export const API_URL = `${API_BASE_URL}${API_VERSION}`;
